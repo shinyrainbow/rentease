d679be8d67b29784c9fb3a54747164621269a09d
shinyrainbow <kornkanok.lieng@gmail.com>
2026-02-03 13:18:36 +0700

    ด

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,6 +1,6 @@
 import { Pool } from "pg";
 import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "@/generated/prisma";
+import { PrismaClient } from "../generated/prisma";
 
 const connectionString = process.env.DATABASE_URL!;
 
