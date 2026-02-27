4acae130f0af62984a338d6e855c8e882afcf47b
shinyrainbow <kornkanok.lieng@gmail.com>
2026-02-27 15:49:16 +0700

    d

--- prisma/migrations/verify-snapshots.sql
+++
@@ -1,7 +0,0 @@
-SELECT 'MeterReading' as "table", COUNT(*) as total, COUNT("projectName") as with_snapshot FROM "MeterReading"
-UNION ALL
-SELECT 'Invoice', COUNT(*), COUNT("projectName") FROM "Invoice"
-UNION ALL
-SELECT 'Payment', COUNT(*), COUNT("projectName") FROM "Payment"
-UNION ALL
-SELECT 'Receipt', COUNT(*), COUNT("projectName") FROM "Receipt";
